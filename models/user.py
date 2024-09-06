4fc337fe6ceca2ade03f4493c64c0451597c4b9f
Abdelrahman Amr <AbdelrahmanAmr.Egypt@gmail.com>
2024-09-07 00:35:33 +0300

    Define user

---
+++ models/user.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" holds class User"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from hashlib import md5
+
+class User(BaseModel, Base):
+    """Representation of a user"""
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    reviews = relationship("Review", backref="user_reviews")
+    sessions = relationship("Session", back_populates="user", cascade="all, delete, delete-orphan")
+
+    def __init__(self, *args, **kwargs):
+        """Initializes user"""
+        super().__init__(*args, **kwargs)
+
+    def __setattr__(self, name, value):
+        """Sets a password with md5 encryption"""
+        if name == "password":
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
