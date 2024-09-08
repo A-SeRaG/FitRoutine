be6bb3ffc67816f43ef8cb046d174f6c8f107b24
Zee-2000 <zahwaabdelnasser@gmail.com>
2024-09-08 22:00:03 +0300

    adding review

--- FitRoutine.sql
+++ FitRoutine.sql
@@ -2,29 +2,37 @@ CREATE DATABASE FitRoutine;
 CREATE TABLE User
 (
     user_id INT not null,
-   first_name VARCHAR(50 not null),
+   first_name VARCHAR(50 ) not NULL,
    last_name VARCHAR(50)not null,
-    date_of_birth date,
-    age INT,
-    phone INT,
-    usr_country VARCHAR(256),
-    gender BIT,
-    usr_weight INT, 
-    usr_height INT,
-        primary key(user_id)
+    date_of_birth date not null,
+    age INT not null,
+    phone INT not null,
+    usr_country VARCHAR(256) not null,
+    gender BIT not null,
+    usr_weight INT not null, 
+    usr_height INT not null,
+    primary key(user_id)
     
 );
 CREATE TABLE IF NOT EXISTS exercise
 (
     exercise_id INT NOT NULL,
-    excercise_name VARCHAR(8),
-    exercise_description VARCHAR(20),
-    target_muscle VARCHAR(128),
-    exercise_sets INT,
-    reps INT,
-    rest_period INT,
-    IsInjury BIT,
+    excercise_name VARCHAR(8) not null,
+    exercise_description VARCHAR(20) not null,
+    target_muscle VARCHAR(128) not null,
+    exercise_sets INT not null,
+    reps INT not null,
+    rest_period INT not null,
+    IsInjury BIT not null,
     primary key(exercise_id)
 
+);
+CREATE TABLE IF NOT EXISTS review
+(
+    exercise_id INT not NULL,
+    user_id INT not NULL,
+    stars INT NOT NULL,
+    text VARCHAR(256) not null,
+    Foreign key(exercise_id)
 );
 
