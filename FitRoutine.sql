5ad8bfcf693c67055b00264ba41f669340df2b6b
Zee-2000 <zahwaabdelnasser@gmail.com>
2024-09-08 21:54:36 +0300

    adding e xercise

--- FitRoutine.sql
+++ FitRoutine.sql
@@ -1,12 +1,30 @@
 CREATE DATABASE FitRoutine;
 CREATE TABLE User
 (
-    user_id INT,
-   first_name VARCHAR(50),
-   last_name VARCHAR(50),
+    user_id INT not null,
+   first_name VARCHAR(50 not null),
+   last_name VARCHAR(50)not null,
     date_of_birth date,
     age INT,
+    phone INT,
+    usr_country VARCHAR(256),
+    gender BIT,
     usr_weight INT, 
     usr_height INT,
+        primary key(user_id)
+    
+);
+CREATE TABLE IF NOT EXISTS exercise
+(
+    exercise_id INT NOT NULL,
+    excercise_name VARCHAR(8),
+    exercise_description VARCHAR(20),
+    target_muscle VARCHAR(128),
+    exercise_sets INT,
+    reps INT,
+    rest_period INT,
+    IsInjury BIT,
+    primary key(exercise_id)
 
 );
+
