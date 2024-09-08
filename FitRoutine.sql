22b54000cd32d8cf32218f754a9e186d1f5eb18b
Zee-2000 <zahwaabdelnasser@gmail.com>
2024-09-08 21:03:07 +0300

    sql tables

---
+++ FitRoutine.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE FitRoutine;
+CREATE TABLE User
+(
+    user_id INT,
+   first_name VARCHAR(50),
+   last_name VARCHAR(50),
+    date_of_birth date,
+    age INT,
+    usr_weight INT, 
+    usr_height INT,
+
+);
