55431f4e512a0268ac2f79ff4e7c8e8068b626fb
SeRaG <aserag0101@gmail.com>
2024-09-12 08:42:09 +0300

    solve 2

--- JS/sessionCheck.js
+++ JS/sessionCheck.js
@@ -1,3 +1,15 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
 function checkSession() {
     const session = document.cookie.split('; ').find(row => row.startsWith('session_id='));
     const user = document.cookie.split('; ').find(row => row.startsWith('user_id='));
